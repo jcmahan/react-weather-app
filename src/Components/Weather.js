4153d161ae3b4d1e02e47ac040c975136525faaa
James Mahan <36861942+jcmahan@users.noreply.github.com>
2018-07-25 18:42:12 -0700

    fixed temp to show in fahrenheit

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Weather = props => (
     <div>
             { props.city && props.country && <p>Location: { props.city }, { props.country } </p> }
-            { props.temperature && <p>Temperature: { props.temperature } </p>}
+            { props.temperature && <p>Temperature: { props.temperature }°F </p>}
             { props.humidity && <p>Humidity: { props.humidity } </p>}
             { props.description && <p>Conditions: { props.description } </p>}
             { props.error && <p> {props.error}</p> }
