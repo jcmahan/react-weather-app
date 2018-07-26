1fc5c26965f7124743525feb88f325ff54a4608e
James Mahan <36861942+jcmahan@users.noreply.github.com>
2018-07-25 18:25:44 -0700

    finished with logic, needs styling help

--- src/App.js
+++ src/App.js
@@ -4,13 +4,56 @@ import Titles from './Components/Titles';
 import Form from './Components/Form';
 import Weather from './Components/Weather';
 
+const API_KEY = 'cea5fdfb316258bb3a2bd15bcd4214f2'; 
+
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined, 
+    description: undefined,
+    error: undefined
+  };
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);
+    const data = await api_call.json(); 
+    if (city && country ) {
+      this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description, 
+      error: '',
+    });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined, 
+        description: undefined,
+        error: "Please enter a City and State!"
+      })
+    }
+  }
   render() {
     return(
       <div> 
         <Titles />
-        <Form />
-        <Weather />
+        <Form getWeather={this.getWeather} />
+        <Weather 
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
